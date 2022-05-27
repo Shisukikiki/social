531a5bcb10d7be88a0ddec7e4efb7bfd05f61a68
Shisukikiki <106232824+Shisukikiki@users.noreply.github.com>
2022-05-27 20:20:30 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  // replace the config
-  //and enable google auth firebase firestore  and the storage
-  //i'm using firebase v9
-  //please like and share my youtube videos and my goal is to reach 2k this year
-  //thanks for the support
-  //also something is wrong with faker.js api with the images
+  apiKey: "AIzaSyD2UbZdNOVhetQvWwIQYSJuK153Ze6amyI",
+  authDomain: "facebook-d8167.firebaseapp.com",
+  projectId: "facebook-d8167",
+  storageBucket: "facebook-d8167.appspot.com",
+  messagingSenderId: "346000138147",
+  appId: "1:346000138147:web:61aec488e9fac36dab0416"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
